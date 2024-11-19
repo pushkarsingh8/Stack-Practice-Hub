de6172c81f2977dd7f66c90dd6a6ab34dfdb71de
Pushkar Singh <2pushkarsingh08>
2024-11-18 16:47:58 -0800

    New DS

---
+++ First Stack inserting data from Head.py
@@ -0,0 +1,40 @@
+#Inserting from Head As Operation (push) In Stack:-
+
+class Node:
+    def __init__(self,value):
+        self.data = value
+        self.next = None
+        
+    
+class stack:
+    def __init__(self):
+        self.top = None
+        
+        
+    def isempty(self):
+        return self.top == None 
+        
+    def push(self,value):
+        new_node = Node(value)
+        new_node.next = self.top
+        
+        self.top = new_node
+        
+        
+    def traverse(self):
+        
+        temp = self.top
+        
+        while temp!=None:
+            print(temp.data)
+            temp = temp.next
+            
+s = stack()
+
+s.push(1)
+s.push(2)
+s.push(3)
+s.push(4)
+
+s.traverse()
+print(s)
