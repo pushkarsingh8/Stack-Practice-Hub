10c62fc722d8607e23e1ab4d40a8edb2272ebccc
Pushkar Singh <2pushkarsingh08>
2024-11-24 18:03:18 -0800

    Text Editior

---
+++ Text editior.py
@@ -0,0 +1,86 @@
+#Text editor using stack:-
+
+class Node:
+    def __init__(self,value):
+        self.data = value
+        self.next = None
+        
+    
+class stack:
+    def __init__(self):
+        self.top = None
+        
+        
+    def isempty(self):
+        return self.top == None 
+        
+    def push(self,value):
+        new_node = Node(value)
+        new_node.next = self.top
+        
+        self.top = new_node
+        
+    def pop(self):
+        #it' remove a last item those on top of stack "LIFO"
+        if self.isempty():
+            return 'stack empty'
+        
+        else:
+            data = self.top.data
+            self.top = self.top.next
+            
+        return data
+            
+    def traverse(self):
+        
+        if self.isempty():
+            print('stack empty')
+        
+        curr = self.top
+        
+        while curr!=None:
+            print(curr.data)
+            curr = curr.next
+            
+class Text_editor():   
+    def __init__(self):
+        self.u = stack()
+        self.r = stack()
+        
+         
+    def txt_editior(self,text,pattern):
+        
+        for char in text:
+            self.u.push(char)
+            
+        for word in pattern:
+            if word == "u":
+                data = self.u.pop()
+                print("3")
+                self.r.push(data)
+            else:
+                data = self.r.pop()
+                print("3")
+                self.u.push(data)
+                
+        result = ''
+        
+        while not self.u.isempty():
+
+            result = self.u.pop() + result 
+            
+        print("Output:",result)
+        
+    
+    
+            
+        
+    
+   
+
+                    
+s = Text_editor()
+s.txt_editior("hello","uru")
+
+
+                    
